27735f85ecfcec1f180ccece07dda0f52af15d96
Marco Deneke <marco.deneke@t-online.de>
2018-10-17 03:10:15 +0200

    Fixed compile errors finished parser migration

--- Assets/Parsing/ImageStack.cs
+++ Assets/Parsing/ImageStack.cs
@@ -3,50 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using DICOMParser;
 
-public class ImageStack : MonoBehaviour {
+namespace DICOMData
+{
 
-    public Dropdown selection;
-    public Progresshandler progresshandler;
-
-    private int[][][] data;
-
-    private List<DiFile> dicomFiles;
-
-    private string folderPath;
-
-    private int width;
-    private int height;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void init()
+    public class ImageStack : MonoBehaviour
     {
-        width = 0;
-        height = 0;
 
-        dicomFiles = new List<DiFile>();
-        var folders = new List<string>(Directory.GetDirectories(Application.streamingAssetsPath));
+        public Dropdown selection;
+        public Progresshandler progresshandler;
 
-        foreach (var fold in folders)
+        private int[][][] data;
+
+        private List<DiFile> dicomFiles;
+
+        private string folderPath;
+
+        private int width;
+        private int height;
+
+        // Use this for initialization
+        void Start()
         {
-            if (fold.Contains(selection.captionText.text))
-            {
-                folderPath = fold;
-            }
-            break;
+
         }
 
-        string[] filePaths = Directory.GetFiles(folderPath);
+        // Update is called once per frame
+        void Update()
+        {
 
-        progresshandler.init(filePaths.Length);
+        }
+
+        public void init()
+        {
+            width = 0;
+            height = 0;
+
+            dicomFiles = new List<DiFile>();
+            var folders = new List<string>(Directory.GetDirectories(Application.streamingAssetsPath));
+
+            foreach (var fold in folders)
+            {
+                if (fold.Contains(selection.captionText.text))
+                {
+                    folderPath = fold;
+                }
+                break;
+            }
+
+            string[] filePaths = Directory.GetFiles(folderPath);
+
+            progresshandler.init(filePaths.Length);
+        }
     }
 }
